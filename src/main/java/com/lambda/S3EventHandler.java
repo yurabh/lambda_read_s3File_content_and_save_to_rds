60267a136a8e2d8c2a55edffc6d02008157cee60
yurabh <yurabh1990@gmail.com>
2023-01-14 17:11:03 +0200

    Move some code in Utils class

--- src/main/java/com/lambda/S3EventHandler.java
+++ src/main/java/com/lambda/S3EventHandler.java
@@ -1,32 +1,19 @@
 package com.lambda;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import com.utils.LambdaUtils;
 
 public class S3EventHandler implements RequestHandler<S3EventNotification, Boolean> {
-
-    public static final String REGION = "us-east-1";
-    public static final String SOURCE_ACCESS_KEY = "AKIATKMOUZAOVHFBT322";
-    public static final String SOURCE_SECRET_KEY = "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";
-    public static final AWSCredentials CREDENTIALS = new BasicAWSCredentials(SOURCE_ACCESS_KEY, SOURCE_SECRET_KEY);
     private static final AmazonS3 amazonS3 = AmazonS3ClientBuilder
             .standard()
-            .withCredentials(new AWSStaticCredentialsProvider(CREDENTIALS))
-            .withRegion(REGION)
+            .withCredentials(new AWSStaticCredentialsProvider(LambdaUtils.CREDENTIALS))
+            .withRegion(LambdaUtils.REGION)
             .build();
 
     public Boolean handleRequest(S3EventNotification s3EventNotification, Context context) {
